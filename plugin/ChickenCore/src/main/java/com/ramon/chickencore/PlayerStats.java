f1c3398881d034a9cb837a3faccb1e8ae27e6308
Paquito12341 <ramoneparedes25@gmail.com>
2026-06-05 17:23:35 +0200

    Minecraft2.0

---
+++ plugin/ChickenCore/src/main/java/com/ramon/chickencore/PlayerStats.java
@@ -0,0 +1,18 @@
+package com.ramon.chickencore;
+
+public class PlayerStats {
+    private int level;
+    private int xp;
+    private int deaths;
+    private int kills;
+    private int blocksBroken;
+
+    public PlayerStats() {
+        this.level = 1;
+        this.xp = 0;
+        this.deaths = 0;
+        this.kills = 0;
+        this.blocksBroken = 0;
+
+    }
+}
