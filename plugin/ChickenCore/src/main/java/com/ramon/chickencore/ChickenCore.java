5118e2f3f13ff7ba49ec79d794a386768ddc5170
Paquito12341 <ramoneparedes25@gmail.com>
2026-06-06 11:52:46 +0200

    Puglin2

--- plugin/ChickenCore/src/main/java/com/ramon/chickencore/ChickenCore.java
+++ plugin/ChickenCore/src/main/java/com/ramon/chickencore/ChickenCore.java
@@ -5,20 +5,44 @@ import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.Location;
+import java.util.HashMap;
+import java.util.UUID;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.entity.EntityType;
 
 
 //Paper crea un objeto Command.
 //Y quien ejecuta el comando se guarda en CommandSender.
 
+
+
 public class ChickenCore extends JavaPlugin implements Listener {
+
+
+
+    private final HashMap<UUID, Location> deathLocations = new HashMap<>();
+    private final HashMap<UUID, PlayerStats> playerStats = new HashMap<>();
+    private PlayerStats test = new PlayerStats();
+    private Location spawnLocation;
+
     //ChickenCore es un plugin de Paper
 
     @Override
     public void onEnable() {
+
+        saveDefaultConfig();
+
+        if (getConfig().contains("spawn")) {
+            spawnLocation = getConfig().getLocation("spawn");
+        }
+
+
         getServer().getPluginManager().registerEvents(this, this);
         getLogger().info("ChickenCore iniciado correctamente");
     }
@@ -40,10 +64,19 @@ public class ChickenCore extends JavaPlugin implements Listener {
     {
         Player player = event.getPlayer();
 
+
+        playerStats.putIfAbsent(
+                player.getUniqueId(),
+                new PlayerStats()
+        );
+
         //Significa:
         //Del evento que acaba de ocurrir
         //Obtén el jugador involucrado
         //Guárdalo en una variable llamada player
+        //Y  tambien Cuando el jugador entra:
+        //si no tiene ficha de stats,
+        //créale una.
 
         player.sendMessage("Bienvenido, " + player.getName() + ".");
 
@@ -112,15 +145,121 @@ public class ChickenCore extends JavaPlugin implements Listener {
 
             player.sendMessage("Bienvenido al servidor.");
             player.sendTitle("BIENVENIDO", player.getName(), 10, 70, 20);
-
-            //10 = aparición
-            //70 = duración
-            //20 = desaparición
-
             player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1.0f, 1.0f);
 
             return true;
         }
+
+        if (command.getName().equalsIgnoreCase("deathcoords")) {
+
+            if (!(sender instanceof Player player)) {
+                sender.sendMessage("Solo jugadores.");
+                return true;
+            }
+
+            if (!deathLocations.containsKey(player.getUniqueId())) {
+                player.sendMessage("Ute' no a muelto, que lo que hace cñ");
+                return true;
+            }
+
+            Location location = deathLocations.get(player.getUniqueId());
+
+            int x = location.getBlockX();
+            int y = location.getBlockY();
+            int z = location.getBlockZ();
+
+            player.sendMessage("Tus últimas coordenadas de muerte: " +
+                    " X: "+ x +
+                    " Y: " + y +
+                    " Z: "+ z );
+
+
+            return true; }
+
+        if (command.getName().equalsIgnoreCase("setspawn")) {
+
+            if (!(sender instanceof Player player)) {
+                sender.sendMessage("Solo jugadores.");
+                return true;
+            }
+
+            spawnLocation = player.getLocation();
+
+            getConfig().set("spawn", spawnLocation);
+            //guarda el spawn en la configuración.
+
+            saveConfig();
+
+            //escribe los cambios físicamente en el archivo.
+            //Después de usar /setspawn, se creará algo en:
+            //server/plugins/ChickenCore/config.yml
+            //Y cuando reinicies, esta línea lo recupera:
+            //spawnLocation = getConfig().getLocation("spawn");
+            //Así /spawn seguirá funcionando aunque cierres y abras el servidor.
+
+            player.sendMessage("Spawn gualdao");
+
+            return true;
+
+        }
+
+        if (command.getName().equalsIgnoreCase("spawn")){
+
+            if (!(sender instanceof Player player)) {
+                sender.sendMessage("Solo jugadores.");
+                return true;
+            }
+
+            if(spawnLocation == null) {
+
+                player.sendMessage("Todavia ute' no tiene spawn");
+                return true;
+            }
+
+            player.teleport(spawnLocation);
+            player.sendMessage("Llegate pal' spawn");
+            return true;}
+
+
+        if (command.getName().equalsIgnoreCase("stats")) {
+
+            if(!(sender instanceof Player player)){
+                /*un chin tarde pa explicar esto, pero practicamente
+                Paper te da "sender" que significa " quien ejecuto el comando
+                puede ser un jugador, la consola o commandblock
+
+                instanceof pregunta a que clase pertenece el objeto
+                instanceof debe ser un player, sender es un player?
+                ! significa negacion, como siempre
+                entonces " si sender NO es un player, le mandamos:*/
+
+                sender.sendMessage("Solo jugadores.");
+                return true;
+
+            }
+
+            PlayerStats stats = playerStats.get(player.getUniqueId());
+
+            if (stats == null) {
+                stats = new PlayerStats();
+                playerStats.put(player.getUniqueId(), stats);
+            }
+
+            player.sendMessage("===== STATS =====");
+            player.sendMessage("Nivel: " + stats.getLevel());
+            player.sendMessage("XP: " + stats.getXp());
+            player.sendMessage("Muertes: " + stats.getDeaths());
+            player.sendMessage("Kills: " + stats.getKills());
+            player.sendMessage("Bloques rotos: " + stats.getBlocksBroken());
+
+            return true;
+
+
+
+
+        }
+
+
 
         return false;
     }
@@ -137,7 +276,76 @@ public class ChickenCore extends JavaPlugin implements Listener {
     //↓
     //Mostramos dónde murió
 
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+
+        Player player = event.getPlayer();
+
+
+        deathLocations.put(player.getUniqueId(), player.getLocation());
+
+        //Guarda:
+        //clave → UUID del jugador
+        //valor → ubicación donde murió
+
+        PlayerStats stats = playerStats.get(player.getUniqueId());
+
+        if (stats != null) {
+            stats.addDeath();
+        }
+
+        player.sendMessage("Te moriste, que pringao");
+
+    }
+
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent event) {
+
+        Player killer = event.getEntity().getKiller();
+
+        if (killer == null) {
+            return;
+        }
+
+        PlayerStats stats = playerStats.get(killer.getUniqueId());
+
+        if (stats == null) {
+            stats = new PlayerStats();
+            playerStats.put(killer.getUniqueId(), stats);
+        }
+
+        EntityType type = event.getEntityType();
+
+        int xpGained = switch (type) {
+            case ZOMBIE -> 3;
+            case SKELETON -> 7;
+            case SPIDER -> 5;
+            case CREEPER -> 7;
+            case ENDERMAN -> 10;
+            case BLAZE -> 15;
+            case WITCH -> 12;
+            case WITHER -> 250;
+            case ENDER_DRAGON -> 500;
+            default -> 1;
+        };
+
+        stats.addKill();
+        stats.addXp(xpGained);
+
+        if (type == EntityType.ENDERMAN ||
+                type == EntityType.WITHER ||
+                type == EntityType.ENDER_DRAGON) {
+
+            killer.sendMessage("Has ganado " + xpGained + " XP por matar a " + type.name() + ".");
+        }
+    }
+
+    
 
 
 
-}
+
+
+
+    }
+
--- plugin/ChickenCore/src/main/java/com/ramon/chickencore/PlayerStats.java
+++ plugin/ChickenCore/src/main/java/com/ramon/chickencore/PlayerStats.java
@@ -1,11 +1,29 @@
 package com.ramon.chickencore;
 
 public class PlayerStats {
+
     private int level;
     private int xp;
     private int deaths;
     private int kills;
     private int blocksBroken;
+    private int attributePoints;
+    private int strength;
+    private int defense;
+    private int speed;
+    private int vitality;
+
+
+
+
+
+    public void addKill() {
+        this.kills++;
+    }
+
+    public void addXp(int amount) {
+        this.xp += amount;
+    }
 
     public PlayerStats() {
         this.level = 1;
@@ -14,5 +32,61 @@ public class PlayerStats {
         this.kills = 0;
         this.blocksBroken = 0;
 
+        this.attributePoints = 0;
+        this.strength = 0;
+        this.defense = 0;
+        this.speed = 0;
+        this.vitality = 0;
     }
+    //constructor
+
+
+    //getters
+    public int getLevel() {
+        return level;
+    }
+
+    public int getXp() {
+        return xp;
+    }
+
+    public int getDeaths() {
+        return deaths;
+    }
+
+    public int getKills() {
+        return kills;
+    }
+
+    public int getBlocksBroken() {
+        return blocksBroken;
+    }
+
+    public void addDeath() {
+        this.deaths++;
+    }
+
+    public int getAttributePoints() {
+        return attributePoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getVitality() {
+        return vitality;
+    }
+
+
+
 }
+
